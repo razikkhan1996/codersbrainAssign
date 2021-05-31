40ffbd95b948b9c6acd61600f2279a161fa962fa
Razik <razik99815@gmail.com>
2021-05-31 17:31:53 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import FetchDetails from './components/FetchDetails';
+
+
+const App = () => {
+  return (
+    <div>
+      <FetchDetails/>
+    </div>
+  )
+}
+
+export default App;
